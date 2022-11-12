a69455b2f86d88ee57c4acc6b271525514e1132d
Ghaith Mefteh <ghaithmeftah@gmail.com>
2022-11-12 21:22:12 +0100

    initial commit

---
+++ app/src/main/java/ensio/ia2/tp2_ghaith_mefteh/ConversionActivity.kt
@@ -0,0 +1,29 @@
+package ensio.ia2.tp2_ghaith_mefteh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import ensio.ia2.tp2_ghaith_mefteh.databinding.ActivityConversionBinding
+
+class ConversionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_conversion)
+        val binding = ActivityConversionBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val nb = intent.getFloatExtra("mantant",0.0f)
+        val sens = intent.getStringExtra("sens")
+        binding.textView.text="$nb"
+        binding.textView3.text= sens
+        binding.button3.setOnClickListener {
+            if (sens == "dt_Euro"){
+                Toast.makeText(this,"l'équivalent de $nb Dt en Euro est :: ${nb * 3.2} Dt",Toast.LENGTH_LONG).show()
+            }else{
+                Toast.makeText(this,"L'équivalent de $nb Euro en DT est :: ${nb / 3.2} Euro",Toast.LENGTH_LONG).show()
+            }
+        }
+
+
+    }
+}
---
+++ app/src/main/java/ensio/ia2/tp2_ghaith_mefteh/CurrencyActivity.kt
@@ -0,0 +1,50 @@
+package ensio.ia2.tp2_ghaith_mefteh
+
+import android.content.Intent
+import android.graphics.Color
+import android.os.Binder
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import ensio.ia2.tp2_ghaith_mefteh.databinding.ActivityCurrencyBinding
+
+class CurrencyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_currency)
+        val binding = ActivityCurrencyBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+            binding.button.setOnClickListener {
+
+                binding.button.setBackgroundColor(Color.DKGRAY)
+                binding.button2.setBackgroundColor(Color.LTGRAY)
+                if (binding.editTextTextPersonName.text.isEmpty()){
+                    Toast.makeText(this,"saisir un mantant",Toast.LENGTH_LONG).show()
+                }else{
+                    val i = Intent(this,ConversionActivity::class.java)
+                    intent.putExtra("mantant",binding.editTextTextPersonName.text.toString().toFloat())
+                    intent.putExtra("sens","dt_Euro")
+                    startActivity(i)
+                }
+
+            }
+        binding.button2.setOnClickListener {
+
+            binding.button2.setBackgroundColor(Color.DKGRAY)
+            binding.button.setBackgroundColor(Color.LTGRAY)
+            if (binding.editTextTextPersonName.text.isEmpty()){
+                Toast.makeText(this,"saisir un mantant",Toast.LENGTH_LONG).show()
+            }else{
+                val i = Intent(this,ConversionActivity::class.java)
+                intent.putExtra("mantant",binding.editTextTextPersonName.text.toString().toFloat())
+                intent.putExtra("sens","Euro_dt")
+                startActivity(i)
+            }
+
+        }
+
+
+
+
+    }
+}
